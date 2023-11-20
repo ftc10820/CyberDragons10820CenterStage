a4c14df52f4a685ba882dab2d3a6a89ab14ebcbf
Aadarsh Senthil <aadhisen2006@gmail.com>
2023-11-20 17:25:21 -0500

    functional op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageTeleOp.java
@@ -0,0 +1,179 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class CenterStageTeleOp extends LinearOpMode {
+
+    public DcMotorEx frontLeft;
+    public DcMotorEx frontRight;
+    public DcMotorEx backLeft;
+    public DcMotorEx backRight;
+
+    public DcMotorEx crane;
+
+    public Servo indexer;
+    public Servo intakeLeft;
+    public Servo intakeRight;
+    public Servo craneAngle;
+    public Servo ramp;
+
+    double speedVal = 1.0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // for the drive train
+            double y = gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * -1.1;
+            double rx = -gamepad1.right_stick_x * 0.75;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeft.setPower(frontLeftPower * speedVal);
+            backLeft.setPower(backLeftPower * speedVal);
+            frontRight.setPower(frontRightPower * speedVal);
+            backRight.setPower(backRightPower * speedVal);
+
+            crane.setPower(gamepad2.left_stick_y);
+
+            if (gamepad2.y) {
+
+                craneAngle.setPosition(1);
+
+            } else if (gamepad2.a) {
+
+                craneAngle.setPosition(0);
+
+            } else if (gamepad2.x) {
+
+                craneAngle.setPosition(0.5);
+
+            }
+
+
+
+            /*
+            if (gamepad1.y) {
+
+                indexer.setPosition(0.6);
+
+            } else if (gamepad1.a) {
+
+                indexer.setPosition(0);
+
+            }
+
+             */
+
+            if (gamepad1.y) {
+
+                intakeLeft.setPosition(0.2);
+                intakeRight.setPosition(0.8);
+
+            } else if (gamepad1.a) {
+
+                intakeLeft.setPosition(0.9);
+                intakeRight.setPosition(0.1);
+
+            }
+
+
+
+
+
+
+
+            /*
+            if (gamepad1.y) {
+
+                ramp.setPosition(0.35);
+
+            } else if (gamepad1.a) {
+
+                ramp.setPosition(0.45);
+
+            }
+
+             */
+
+            if (gamepad1.x) {
+
+                ramp.setPosition(0.45);
+                indexer.setPosition(0);
+                sleep(500);
+
+                intakeLeft.setPosition(0);
+                intakeRight.setPosition(0.1);
+                sleep(500);
+
+                indexer.setPosition(1);
+                sleep(1000);
+
+                ramp.setPosition(0.3);
+                indexer.setPosition(0.3);
+                intakeLeft.setPosition(1);
+                intakeRight.setPosition(0.8);
+            }
+
+
+
+
+
+        }
+
+    }
+
+    public void initialize() {
+
+
+        // setting up drive train
+        frontRight = hardwareMap.get(DcMotorEx.class, "FrontRight");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "FrontLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "BackRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "BackLeft");
+
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+
+        frontRight.setDirection(DcMotorEx.Direction.REVERSE);
+        backRight.setDirection(DcMotorEx.Direction.REVERSE);
+
+
+
+        //crane motor
+        crane = hardwareMap.get(DcMotorEx.class, "Crane");
+
+        //servos
+        indexer = hardwareMap.get(Servo.class, "Indexer");
+        intakeLeft = hardwareMap.get(Servo.class, "IntakeLeft");
+        intakeRight = hardwareMap.get(Servo.class, "IntakeRight");
+        craneAngle = hardwareMap.get(Servo.class, "CraneAngle");
+        ramp = hardwareMap.get(Servo.class, "Ramp");
+
+        ramp.setPosition(0.3);
+        indexer.setPosition(0.3);
+        intakeLeft.setPosition(1);
+        intakeRight.setPosition(0.8);
+
+
+
+
+    }
+}
